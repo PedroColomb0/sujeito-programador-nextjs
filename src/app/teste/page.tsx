5f3bb5e59eeea79cb62b8f3530c461e5e5dfd7b3
PedroColomb0 <pedrocolombodev@gmail.com>
2025-12-15 11:24:49 -0400

    client component

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -20,13 +20,16 @@ export default function pageTeste() {
 
             <div>
                 {post.map(post => (
-                    <div key={post.id}>
-                        <h2>{post.title}</h2>
-                        <p>{post.body}</p>
-                    </div>
+                    // <div key={post.id}>
+                    //     <h2>{post.title}</h2>
+                    //     <p>{post.body}</p>
+                    // </div>
                 ))}
             </div>
         </div>
     </div>
   )
+  
 }
+
+
