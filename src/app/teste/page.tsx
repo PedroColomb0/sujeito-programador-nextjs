9f55216b23ed1e48c7e16e104a745faef4f81af9
PedroColomb0 <pedrocolombodev@gmail.com>
2025-12-15 11:24:35 -0400

    client component

---
+++ src/app/teste/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react"
+
+export default function pageTeste() {
+
+    const [post, setPosts] = useState([])
+
+    useEffect((() => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => setPosts(data.posts))
+        
+    }), [])
+
+  return (
+    <div>
+        <div>
+            <h1>Pagina Client</h1>
+
+            <div>
+                {post.map(post => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <p>{post.body}</p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
